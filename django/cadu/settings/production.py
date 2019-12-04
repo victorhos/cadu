0080a78eee41af42d081e534e406e287cb5389ed
Victor Hugo <victor.hos@gmail.com>
2019-12-03 22:15:26 -0300

    add database

--- django/cadu/settings/production.py
+++ django/cadu/settings/production.py
@@ -1,3 +1,14 @@
+import os
+
 from cadu.settings.base import *  # noqa
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DATABASE_NAME', 'cadu'),
+        'USER': os.environ.get('MYSQL_USER', 'root'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD', '')
+    }
+}
